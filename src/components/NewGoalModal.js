5df0f5c084ec49aae9eec290cbab6409c5a3e4ea
Brett Commandeur <bcmdr.co@gmail.com>
2019-11-11 22:43:41 -0700

    autofocus new goal input

--- src/components/NewGoalModal.js
+++ src/components/NewGoalModal.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 export default function NewGoalModal(props) {
+  const titleInput = useRef()
+  useEffect(()=>{
+    titleInput.current.focus();
+  })
   return (
     <section className={"NewGoalModal" + (props.shown ? '' : ' displayNone')}>
       <form autoComplete="off" onSubmit={props.handleSubmit}>
           <label htmlFor="title">Goal Title</label>
-          <input id="title" placeholder="New Goal..." onChange={props.handleChange} value={props.title}></input>
+          <input 
+            ref={titleInput}
+            id="title" placeholder="New Goal..." 
+            onChange={props.handleChange} 
+            value={props.title}>
+          </input>
           <button className="App-button" onClick={props.handleSubmit}>Add</button>
       </form>
     </section>
